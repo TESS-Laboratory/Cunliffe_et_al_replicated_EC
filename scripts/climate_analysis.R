7263d0be2f2020c068486f0372142b36895c9d98
AndrewCunliffe <andrewmcunliffe@gmail.com>
2021-04-28 17:20:22 +0100

    Updated precipitation analysis

--- scripts/climate_analysis.R
+++ scripts/climate_analysis.R
@@ -1,55 +1,56 @@
 ## This script contains the climate analysis
 ## Andrew Cunliffe <andrewmcunliffe@gmail.com>
 ## Started 2021-04-15
-## Using data sourced from https://sevlter.unm.edu/content/meteorology-data-sevilleta-national-wildlife-refuge-new-mexico-1988-present
+
+
 
 
 # ------ 0 Setup Environment ----------
 
-# Install packages
+## Install packages
 library(tidyverse) # For dplyr and ggplot2
 library(viridis) # friendly colour palette
 library(patchwork) # for multi-panel plots
 
 
 #-------------- 1. Extract Data --------------
+## Using data sourced from Moore, D.I. 2021. Meteorology Data from the Sevilleta 
+## National Wildlife Refuge, New Mexico ver 14. Environmental Data Initiative. 
+## https://doi.org/10.6073/pasta/1cbc37ae4d40b3844b5e4be9f6f18073 
+## (Accessed 2021-04-28).
 
-# NB. differences in these files prevent batch reading and merging. 
+## For more information see: https://unmsevilletafieldstation.wordpress.com/met-station-network/
 
-df_1988_1995 <- read_csv("data/meteorological_data/sev1_meteorology_1988-1995.txt")
-df_1996_2000 <- read_csv("data/meteorological_data/sev1_meteorology_1996-2000.txt")
+## Read data
+df_1988_1994 <- read_csv("data/meteorological_data/Sevilleta_LTER_Hourly_Meteorological_Data_1988_1994.csv")
+df_1995_1999 <- read_csv("data/meteorological_data/Sevilleta_LTER_Hourly_Meteorological_Data_1995_1999.csv")
+df_2000_2004 <- read_csv("data/meteorological_data/Sevilleta_LTER_Hourly_Meteorological_Data_2000_2004.csv")
+df_2005_2009 <- read_csv("data/meteorological_data/Sevilleta_LTER_Hourly_Meteorological_Data_2005_2009.csv")
+df_2010_2014 <- read_csv("data/meteorological_data/Sevilleta_LTER_Hourly_Meteorological_Data_2010_2014.csv")
+df_2015_2019 <- read_csv("data/meteorological_data/Sevilleta_LTER_Hourly_Meteorological_Data_2015_2019.csv")
+df_2020 <- read_csv("data/meteorological_data/Sevilleta_LTER_Hourly_Meteorological_Data_2020.csv")
 
-# NB. The header is missing from the 2001-2005 file, so column names were copied from the first file
-col_names <- names(df_1988_1995)
-df_2001_2005 <- read_csv("data/meteorological_data/sev1_meteorology_2001-2005.txt", col_names = col_names)
-
-df_2006_2010 <- read_csv("data/meteorological_data/sev1_meteorology_2006-2010.txt")
-
-# NB. The header of this file has been shifted off by one place', so I've skipped the header row and recycled column headers
-df_2011_2015 <- read_csv("data/meteorological_data/sev1_meteorology_2011-2015.txt", skip = 1, col_names = col_names)
-
-
-# Combine data
-df <- rbind(df_1988_1995, df_1996_2000, df_2001_2005, df_2006_2010, df_2011_2015)
-
-# 
-rm(df_1988_1995, df_1996_2000, df_2001_2005, df_2006_2010, df_2011_2015)
-
+# Annual summaries extracted from Fluxnet files (as part of JULES prep. script)
+AnnualPrecip_Seg <- read_csv("data/meteorological_data/annual_precip_Seg.csv")
+AnnualPrecip_Sen <- read_csv("data/meteorological_data/annual_precip_Sen.csv")
+AnnualPrecip_Ses <- read_csv("data/meteorological_data/annual_precip_Ses.csv")
 
 
+## Combine data
+df <- rbind(df_1988_1994, df_1995_1999, df_2000_2004, df_2005_2009, df_2010_2014, df_2015_2019, df_2020)
+
+rm(df_1988_1994, df_1995_1999, df_2000_2004, df_2005_2009, df_2010_2014, df_2015_2019, df_2020)  # Remove unnecessary objects 
+
+
+## Review dataset summary
+# summary(df)
+names(df)
 
 # -------------- 2. Tidy Data --------------
-
-# Replace error codes with NA values (where '-999' = missing values and '-888' = missing sensor)
-df <- df %>%
-  dplyr::na_if(-999) %>% 
-  dplyr::na_if(-888)
-
-
-
-# Select variables of interest
+## Select variables of interest
 df2 <- df %>% 
   select(StationID,
+         Date_Time,
          Year,
          Julian_Day,
          Hour,
@@ -61,8 +62,8 @@ df2 <- df %>%
 
 # -------------- 3. Analyze Data --------------
 
-# The longest running station is StationID = 40, which has been running since mid 1987.
-# Station 40 is still pretty close to our study site and is representative for annual totals. 
+## StationID = 40 (Deep Well) is the longest running station, active since mid 1987.
+## Station 40 is within a few km of our study sites and we consider it representative for annual totals. 
 
 
 ### Extract annual statistics ###
@@ -77,7 +78,7 @@ AnnualTemp_Station40 <- df2 %>%
   summarise(Temp_C = mean(Temp_C, na.rm = TRUE))
 
 
-# The closest station is StationID = 49 (Five Points Meteorological Station but this only started in 1999.
+# StationID = 49 (Five Points Meteorological Station) is closer to the Ses site but only started in 1999.
 AnnualPrecip_Station49 <- df2 %>% 
   filter(StationID == 49) %>% 
   group_by(Year) %>% 
@@ -88,6 +89,25 @@ AnnualTemp_Station49 <- df2 %>%
   group_by(Year) %>% 
   summarise(Temp_C = mean(Temp_C, na.rm = TRUE))
 
+
+### Add Station ID to precipitation records
+AnnualPrecip_Station40 <- AnnualPrecip_Station40 %>% 
+  mutate(Station = "Station40")
+
+AnnualPrecip_Station49 <- AnnualPrecip_Station49 %>% 
+  mutate(Station = "Station49")
+
+AnnualPrecip_Seg <- AnnualPrecip_Seg %>% 
+  mutate(Station = "Seg") %>% 
+  rename(Year = year)
+
+AnnualPrecip_Sen <- AnnualPrecip_Sen %>% 
+  mutate(Station = "Sen") %>% 
+  rename(Year = year)
+
+AnnualPrecip_Ses <- AnnualPrecip_Ses %>% 
+  mutate(Station = "Ses") %>% 
+  rename(Year = year)
 
 
 ### Extract mean annual statistics ###
@@ -104,21 +124,102 @@ AnnualTemp_Station49 <- df2 %>%
 (iqr_ID40 <- IQR(AnnualPrecip_Station40$Precipitation))
 (iqr_ID49 <- IQR(AnnualPrecip_Station49$Precipitation))
 
-
+## Calculate Coefficient of Variation
+(CoV_ID40 <- sd(AnnualPrecip_Station40$Precipitation)/mean_precip_ID40)
+(CoV_ID49 <- sd(AnnualPrecip_Station49$Precipitation)/mean_precip_ID49)
 
 
 # -------------- 4. Visualize Data --------------
 
-(precip_ID40 <- ggplot(AnnualPrecip_Station40, aes(x=Year, y=Precipitation)) +
-    labs(x = "Year", y = "Annual Precipitation (mm)", title = "Station 40") +
-    geom_point(aes(x=Year, y=Precipitation)) +
-    geom_line(aes(y = mean_precip_ID40)) +
-    geom_line(aes(y = (mean_precip_ID40-(iqr_ID40/2))), linetype = "dashed") +
-    geom_line(aes(y = (mean_precip_ID40+(iqr_ID40/2))), linetype = "dashed") +
-   coord_cartesian(xlim=c(1987,2020), ylim=c(0, 550)) +
-   theme_bw() +
-    theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
-  )
-  
 
+# (precip_ID40 <- ggplot(AnnualPrecip_Station40, aes(x=Year, y=Precipitation)) +
+#    labs(x = "Year", y = "Annual Precipitation (mm)", title = "Precipitation at Station 40") +
+#    geom_point(aes(x=Year, y=Precipitation)) +
+#    geom_line(aes(y = mean_precip_ID40)) +
+#    geom_line(aes(y = (mean_precip_ID40-(iqr_ID40/2))), linetype = "dashed") +
+#    geom_line(aes(y = (mean_precip_ID40+(iqr_ID40/2))), linetype = "dashed") +
+#    geom_point(aes(x=2019, y=517, colour="Red")) +
+#    geom_point(aes(x=2019, y=458, colour="blue")) +
+#    coord_cartesian(xlim=c(1988,2020), ylim=c(0, 550)) +
+#    theme_bw() +
+#    theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+# )
+
+# (precip_ID40 <- ggplot(AnnualPrecip_Station40, aes(x=Year, y=Precipitation)) +
+#    labs(title = "Precipitation at Station 40") +
+#    geom_point(aes(x=Year, y=Precipitation)) +
+#     geom_line(aes(y = mean_precip_ID40)) +
+#     geom_line(aes(y = (mean_precip_ID40-(iqr_ID40/2))), linetype = "dashed") +
+#     geom_line(aes(y = (mean_precip_ID40+(iqr_ID40/2))), linetype = "dashed") +
+#    geom_point(aes(x=2019, y=517, colour="Red")) +
+#    geom_point(aes(x=2019, y=458, colour="blue")) +
+#    scale_x_continuous(name="Year", breaks=seq(1990,2020,10), limits=c(1988,2020)) +
+#    scale_y_continuous(name="Annual Precipitation (mm)", breaks=seq(0,550,50), limits=c(0, 550)) +
+#    theme_bw() +
+#     theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+#   )
+#   
+# ggsave("plots/Annual precipitation at Station 40.png", 
+#        precip_ID40,
+#        width=12,
+#        height=12,
+#        units="cm")
+
+
+# (precip_ID49 <- ggplot(AnnualPrecip_Station49, aes(x=Year, y=Precipitation)) +
+#     labs(title = "Precipitation at Station 49") +
+#     geom_point(aes(x=Year, y=Precipitation)) +
+#     geom_line(aes(y = mean_precip_ID49)) +
+#     geom_line(aes(y = (mean_precip_ID49-(iqr_ID40/2))), linetype = "dashed") +
+#     geom_line(aes(y = (mean_precip_ID49+(iqr_ID40/2))), linetype = "dashed") +
+#     geom_point(aes(x=2019, y=517, colour="Red")) +
+#     geom_point(aes(x=2019, y=458, colour="blue")) +
+#     scale_x_continuous(name="Year", breaks=seq(1990,2020,10), limits=c(1988,2020)) +
+#     scale_y_continuous(name="Annual Precipitation (mm)", breaks=seq(0,550,50), limits=c(0, 550)) +
+#     theme_bw() +
+#     theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+# )
+# 
+# ggsave("plots/Annual precipitation at Station 49.png", 
+#        precip_ID49,
+#        width=12,
+#        height=12,
+#        units="cm")
+
+
+## Manually add our stated precipitation totals
+Stated_Seg <- data.frame(Year=2019, Precipitation=517, Station="Stated_Seg")
+Stated_Ses <- data.frame(Year=2019, Precipitation=458, Station="Stated_Ses")
+
+
+### Combine annual precipitation records from all stations
+AnnualPrecip <- rbind(AnnualPrecip_Station40,
+                      AnnualPrecip_Station49,
+                      AnnualPrecip_Seg,
+                      AnnualPrecip_Sen,
+                      AnnualPrecip_Ses,
+                      Stated_Seg,
+                      Stated_Ses
+                      )
+
+
+## Plot annual precipitation from all stations
+(AnnualPrecip_plots <- ggplot(AnnualPrecip, aes(x=Year, y=Precipitation, group=Station)) +
+    labs(title = "Annual precipitation at different stations") +
+    geom_point(aes(x=Year, y=Precipitation, colour=Station)) +
+    geom_line(aes(y = mean_precip_ID40)) +
+    geom_line(aes(y = (mean_precip_ID49-(iqr_ID40/2))), linetype = "dotted") +
+    geom_line(aes(y = (mean_precip_ID49+(iqr_ID40/2))), linetype = "dotted") +
+    scale_colour_viridis_d(option = "plasma") +
+    scale_x_continuous(name="Year", breaks=seq(1990,2020,10), limits=c(1988,2020)) +
+    scale_y_continuous(name="Annual Precipitation (mm)", breaks=seq(0,550,50), limits=c(0, 550)) +
+    theme_bw() +
+    theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+)
+
+ggsave("plots/Annual precipitation.png", 
+       AnnualPrecip_plots,
+       width=16,
+       height=12,
+       units="cm")
 
