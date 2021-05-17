389dd1b7ad8bf83f6f0e154c08c3dc4bc335fa8b
AndrewCunliffe <andrewmcunliffe@gmail.com>
2021-05-17 11:09:57 +0100

    Update climate analysis plots. Now looking good

--- scripts/climate_analysis.R
+++ scripts/climate_analysis.R
@@ -4,7 +4,6 @@
 
 
 
-
 # ------ 0 Setup Environment ----------
 
 ## Install packages
@@ -22,13 +21,47 @@ library(patchwork) # for multi-panel plots
 ## For more information see: https://unmsevilletafieldstation.wordpress.com/met-station-network/
 
 ## Read data
-df_1988_1994 <- read_csv("data/meteorological_data/Sevilleta_LTER_Hourly_Meteorological_Data_1988_1994.csv")
-df_1995_1999 <- read_csv("data/meteorological_data/Sevilleta_LTER_Hourly_Meteorological_Data_1995_1999.csv")
-df_2000_2004 <- read_csv("data/meteorological_data/Sevilleta_LTER_Hourly_Meteorological_Data_2000_2004.csv")
-df_2005_2009 <- read_csv("data/meteorological_data/Sevilleta_LTER_Hourly_Meteorological_Data_2005_2009.csv")
-df_2010_2014 <- read_csv("data/meteorological_data/Sevilleta_LTER_Hourly_Meteorological_Data_2010_2014.csv")
-df_2015_2019 <- read_csv("data/meteorological_data/Sevilleta_LTER_Hourly_Meteorological_Data_2015_2019.csv")
-df_2020 <- read_csv("data/meteorological_data/Sevilleta_LTER_Hourly_Meteorological_Data_2020.csv")
+df_1988_1994 <- read_csv("data/meteorological_data/Sevilleta_LTER_Hourly_Meteorological_Data_1988_1994.csv",
+                         col_types = cols(Evaporation = col_double(),
+                                          Evap_Pan_Temperature = col_double(),
+                                          Bar_Pressure = col_double()))
+
+df_1995_1999 <- read_csv("data/meteorological_data/Sevilleta_LTER_Hourly_Meteorological_Data_1995_1999.csv",
+                         col_types = cols(Moisture_10_cm = col_double(),
+                                          Moisture_30_cm  = col_double(),
+                                          Evaporation = col_double(),
+                                          Evap_Pan_Temperature  = col_double()))
+
+df_2000_2004 <- read_csv("data/meteorological_data/Sevilleta_LTER_Hourly_Meteorological_Data_2000_2004.csv",
+                         col_types = cols(Moisture_10_cm = col_double(),
+                                          Moisture_30_cm  = col_double(),
+                                          Evaporation = col_double(),
+                                          Evap_Pan_Temperature  = col_double()))
+
+df_2005_2009 <- read_csv("data/meteorological_data/Sevilleta_LTER_Hourly_Meteorological_Data_2005_2009.csv",
+                         col_types = cols(Moisture_10_cm = col_double(),
+                                          Moisture_30_cm  = col_double(),
+                                          Evaporation = col_double(),
+                                          Evap_Pan_Temperature  = col_double()))
+
+df_2010_2014 <- read_csv("data/meteorological_data/Sevilleta_LTER_Hourly_Meteorological_Data_2010_2014.csv",
+                         col_types = cols(Moisture_10_cm = col_double(),
+                                          Moisture_30_cm  = col_double(),
+                                          Evaporation = col_double(),
+                                          Evap_Pan_Temperature  = col_double()))
+
+
+df_2015_2019 <- read_csv("data/meteorological_data/Sevilleta_LTER_Hourly_Meteorological_Data_2015_2019.csv",
+                         col_types = cols(Moisture_10_cm = col_double(),
+                                          Moisture_30_cm  = col_double(),
+                                          Evaporation = col_double(),
+                                          Evap_Pan_Temperature  = col_double()))
+
+df_2020 <- read_csv("data/meteorological_data/Sevilleta_LTER_Hourly_Meteorological_Data_2020.csv",
+                    col_types = cols(Moisture_10_cm = col_double(),
+                                     Moisture_30_cm  = col_double(),
+                                     Evaporation = col_double(),
+                                     Evap_Pan_Temperature  = col_double()))
 
 # Annual summaries extracted from Fluxnet files (as part of JULES prep. script)
 AnnualPrecip_Seg <- read_csv("data/meteorological_data/annual_precip_Seg.csv")
@@ -42,9 +75,6 @@ df <- rbind(df_1988_1994, df_1995_1999, df_2000_2004, df_2005_2009, df_2010_2014
 rm(df_1988_1994, df_1995_1999, df_2000_2004, df_2005_2009, df_2010_2014, df_2015_2019, df_2020)  # Remove unnecessary objects 
 
 
-## Review data summary
-# summary(df)
-# names(df)
 
 # -------------- 2. Tidy Data --------------
 ## Select variables of interest
@@ -188,8 +218,8 @@ AnnualPrecip_Ses <- AnnualPrecip_Ses %>%
 
 
 ## Manually add our stated precipitation totals
-Stated_Seg <- data.frame(Year=2019, Precipitation=517, Station="Stated_Seg")
-Stated_Ses <- data.frame(Year=2019, Precipitation=458, Station="Stated_Ses")
+# Stated_Seg <- data.frame(Year=2019, Precipitation=179, Station="Seg_Study_Period")
+# Stated_Ses <- data.frame(Year=2019, Precipitation=171, Station="Ses_Study_Period")
 
 
 ### Combine annual precipitation records from all stations
@@ -197,9 +227,7 @@ AnnualPrecip <- rbind(AnnualPrecip_Station40,
                       AnnualPrecip_Station49,
                       AnnualPrecip_Seg,
                       AnnualPrecip_Sen,
-                      AnnualPrecip_Ses,
-                      Stated_Seg,
-                      Stated_Ses
+                      AnnualPrecip_Ses
                       )
 
 
@@ -207,13 +235,19 @@ AnnualPrecip <- rbind(AnnualPrecip_Station40,
 (AnnualPrecip_plots <- ggplot(AnnualPrecip, aes(x=Year, y=Precipitation, group=Station)) +
     labs(title = "Annual precipitation at different stations") +
     geom_point(aes(x=Year, y=Precipitation, colour=Station)) +
-    geom_line(aes(y = mean_precip_ID40)) +
-    geom_line(aes(y = (mean_precip_ID49-(iqr_ID40/2))), linetype = "dotted") +
-    geom_line(aes(y = (mean_precip_ID49+(iqr_ID40/2))), linetype = "dotted") +
+    geom_hline(yintercept = mean_precip_ID40) +
+    geom_hline(yintercept = (mean_precip_ID40+(iqr_ID40/2)), linetype = "dotted") +
+    geom_hline(yintercept = (mean_precip_ID40-(iqr_ID40/2)), linetype = "dotted") +
     scale_colour_viridis_d(option = "plasma") +
     scale_x_continuous(name="Year", breaks=seq(1990,2020,10), limits=c(1988,2020)) +
-    scale_y_continuous(name="Annual Precipitation (mm)", breaks=seq(0,550,50), limits=c(0, 550)) +
+    scale_y_continuous(name="Annual Precipitation (mm)", breaks=seq(0,350,50), limits=c(0, 350)) +
+    # Add annotation for study periods
+    geom_text(x=2024.2, y=179, label="Seg Study Period", color="Red", size=2.1, fontface="bold") +
+    geom_text(x=2024.2, y=171, label="Ses Study Period", color="Red", size=2.1, fontface="bold") +
     theme_bw() +
+    theme(plot.margin = unit(c(1, 4, 1, 1), "lines")) +
+    coord_cartesian(clip = "off") +
+    theme(legend.position = "bottom") +
     theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
 )
 
